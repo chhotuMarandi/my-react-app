7e5fbe3d2b2d85ae6c39ed71664d5b58ec55fc17
chhotu marandi <cmarandi29@gmail.com>
2023-05-20 22:58:33 +0530

    create component challenge

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom'
 
 function Page () {
   return (
-    <ol>
-      <li>I am excited to learning React</li>
-      <li>Because it is the most using framework</li>
-      <li>And to a better developer</li>
-    </ol>
-  )
+    <div>
+      <header>
+        <nav>
+          <img src={require("./logo/react-logo.png")} width={100} />
+        </nav>
+      </header>
+      <h1>I am excited to learn React</h1>
+      <ol>
+        <li>I am excited to learning React</li>
+        <li>Because it is the most using framework</li>
+        <li>And to a better developer</li>
+      </ol>
+      <footer>
+        <p> © All Right Reserved by Chotu Marandi</p>
+      </footer>
+    </div>
+  );
 }
 
 ReactDOM.render(<Page />, document.getElementById('root'))
