bc75f36f33f6d3a755eca5ce4b5770494755556f
chhotu marandi <cmarandi29@gmail.com>
2023-05-20 22:45:24 +0530

    Components

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const page = (
-  <div>
-    <img src={require("./logo/react-logo.png")} width={100}/>
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>was first released in 2013</li>
-      <li>was originally created by Jordan Walke</li>
-      <li>Has well over 100k stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise app, including mobile apps</li>
-    </ul>
-  </div>
-);
+function Page () {
+  return (
+    <ol>
+      <li>I am excited to learning React</li>
+      <li>Because it is the most using framework</li>
+      <li>And to a better developer</li>
+    </ol>
+  )
+}
 
-ReactDOM.render(page, document.getElementById('root'))
+ReactDOM.render(<Page />, document.getElementById('root'))
